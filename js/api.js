08db2f1dab7c1e0e3cb20eeb4f8de3da900c7a38
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-08-29 20:31:30 +0600

    practice

--- js/api.js
+++ js/api.js
@@ -1,7 +1,13 @@
-function loadData() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(data => console.log(data))
+// function loadData() {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+// }
+
+function loadInfo(){
+    fetch ('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(info=>console.log (info))
 }
 
 function loadUsers() {
