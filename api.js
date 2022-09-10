d79e04595d4bd95f781e02027d128ac0d680e435
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-09-10 21:00:10 +0600

    data fetch

---
+++ api.js
@@ -0,0 +1,7 @@
+// PH 33-3 JSON placeholder, GET data, display data on UI
+
+function loadData (){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json.title))
+}
