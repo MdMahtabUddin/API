47b84400d004f10b3b6f8693a2142d92a327e44c
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-09-10 23:58:44 +0600

    added display data

--- api.js
+++ api.js
@@ -6,3 +6,23 @@ function loadData (){
   .then(json => console.log(json.title))
 }
 
+// 33-5 Dynamically display loaded data on your website
+
+function loadUser(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response =>response.json() )
+  .then(data =>displayUsers(data))
+  
+}
+
+function displayUsers(data){
+const ul = document.getElementById('users')
+  for(const user of data){
+    // console.log(user.name)
+    const li = document.createElement('li')
+    li.innerText =`name: ${user.name} ,
+     email: ${user.email}`
+    ul.appendChild(li);
+  }
+}
+
